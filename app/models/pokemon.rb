29d0cb168969d14b682e0cd9f630fad12c095da9
JPlante9117 <jacrplante@gmail.com>
2019-11-26 15:13:34 -0500

    added seed and table type formatting

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -6,7 +6,96 @@ class Pokemon < ActiveRecord::Base
 
     extend Slugifiable::ClassMethods
 
+    @@types = ["Normal", "Fire", "Water", "Electric", "Grass", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon", "Steel", "Dark", "Fairy"]
+
+
     def slug
         self.species.gsub(" ", "-").downcase
     end
+
+    def coloredtable1
+        type = @@types.find {|type| type == self.type1}
+        case type
+        when "Normal"
+          "rgb(226, 226, 226)"
+        when "Fire"
+          "rgb(224, 91, 91)"
+        when "Water"
+          "rgb(91, 137, 224)"
+        when "Electric"
+          "rgb(231, 243, 127)"
+        when "Grass"
+          "rgb(137, 243, 127)"
+        when "Ice"
+          "rgb(157, 248, 248)"
+        when "Fighting"
+          "rgb(245, 159, 78)"
+        when "Poison"
+          "rgb(167, 114, 223)"
+        when "Ground"
+          "tan"
+        when "Flying"
+          "rgb(212, 211, 209)"
+        when "Psychic"
+          "rgb(218, 88, 207)"
+        when "Bug"
+          "rgb(173, 197, 117)"
+        when "Rock"
+          "rgb(160, 138, 96)"
+        when "Ghost"
+          "rgb(130, 100, 163)"
+        when "Dragon"
+          "rgb(79, 82, 238)"
+        when "Fairy"
+          "rgb(246, 128, 191)"
+        when "Steel"
+          "rgb(180, 180, 180)"
+        when "Dark"
+          "black"
+        end
+      end
+
+      def coloredtable2
+        type = @@types.find {|type| type == self.type2}
+        case type
+        when "Normal"
+          "rgb(226, 226, 226)"
+        when "Fire"
+          "rgb(224, 91, 91)"
+        when "Water"
+          "rgb(91, 137, 224)"
+        when "Electric"
+          "rgb(231, 243, 127)"
+        when "Grass"
+          "rgb(137, 243, 127)"
+        when "Ice"
+          "rgb(157, 248, 248)"
+        when "Fighting"
+          "rgb(245, 159, 78)"
+        when "Poison"
+          "rgb(167, 114, 223)"
+        when "Ground"
+          "tan"
+        when "Flying"
+          "rgb(212, 211, 209)"
+        when "Psychic"
+          "rgb(218, 88, 207)"
+        when "Bug"
+          "rgb(173, 197, 117)"
+        when "Rock"
+          "rgb(160, 138, 96)"
+        when "Ghost"
+          "rgb(130, 100, 163)"
+        when "Dragon"
+          "rgb(79, 82, 238)"
+        when "Fairy"
+          "rgb(246, 128, 191)"
+        when "Steel"
+          "rgb(180, 180, 180)"
+        when "Dark"
+          "black"
+        when "None"
+            "rgb(245, 255, 211)"
+        end
+      end
 end
